a07cc7699ae153d1e809f08680bdd852caa00ba9
Bruno Bernardi <bruno.bernardi@celfocus.com>
2025-01-13 14:18:43 +0000

    Resource patch fix

--- src/domain/resource/resource.service.ts
+++ src/domain/resource/resource.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Resource } from './entities/resource.entity';
 
 @Injectable()
@@ -8,6 +8,7 @@ export class ResourceService {
   constructor(
     @InjectRepository(Resource)
     private resourceRepository: Repository<Resource>,
+    private dataSource: DataSource,
   ) {}
 
   findAll(): Promise<Resource[]> {
@@ -19,7 +20,18 @@ export class ResourceService {
     updateData: Partial<Resource>,
   ): Promise<Resource> {
     const id: number = parseInt(idString);
-    await this.resourceRepository.update(id, updateData);
+
+    if (!updateData || Object.keys(updateData).length === 0) {
+      throw new BadRequestException('No update data provided');
+    }
+
+    await this.dataSource
+      .createQueryBuilder()
+      .update(Resource)
+      .set(updateData)
+      .where('id = :id', { id })
+      .execute();
+
     return this.resourceRepository.findOne({ where: { id } });
   }
 }
