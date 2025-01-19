f95b6a9eba9081a0ef70c8a709603e59371fc102
Curacerto <curacerto@gmail.com>
2025-01-19 16:20:59 +0000

    Admin entity

--- src/domain/admin/admin.controller.ts
+++ src/domain/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Patch } from '@nestjs/common';
 import { AdminService } from './admin.service';
 
 @Controller('admin')
@@ -6,9 +6,9 @@ export class AdminController {
   constructor(private readonly adminService: AdminService) {
   }
 
-  @Patch(':id')
-  create(@Param('id') id: string, @Body() msg_id: string) {
-    return this.adminService.updateMsgId(Number.parseInt(id), msg_id);
+  @Patch()
+  create(@Body() msg_id: string) {
+    return this.adminService.updateMsgId(msg_id);
   }
 
   @Get()
--- src/domain/admin/admin.service.ts
+++ src/domain/admin/admin.service.ts
@@ -6,15 +6,17 @@ import { Admin } from './entities/admin.entity';
 @Injectable()
 export class AdminService {
 
+  private readonly adminId: string = 'ADMIN';
+
   constructor(
     @InjectRepository(Admin)
     private adminRepository: Repository<Admin>,
   ) {
   }
 
-  async updateMsgId(id: number, msgId: string): Promise<Admin> {
-    await this.adminRepository.update(id, { msg_id: msgId });
-    return this.adminRepository.findOne({ where: { id } });
+  async updateMsgId(msgId: string): Promise<Admin> {
+    await this.adminRepository.update({ admin_id: this.adminId }, { msg_id: msgId });
+    return this.adminRepository.findOne({ where: { admin_id: this.adminId } });
   }
 
   async findAll(): Promise<Admin[]> {
