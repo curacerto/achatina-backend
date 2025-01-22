d2c10849817af615e1a86755b39723f756993402
Curacerto <curacerto@gmail.com>
2025-01-22 08:06:54 +0000

    Kit operation

--- src/domain/kit/kit.module.ts
+++ src/domain/kit/kit.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Kit } from './entities/kit.entity';
 import { KitController } from './kit.controller';
 import { KitService } from './kit.service';
+import { KitItem } from './entities/kit-item.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Kit]),
+    TypeOrmModule.forFeature([KitItem]),
   ],
   controllers: [KitController],
   providers: [KitService],
