ff915b410e4b4d9ee3eb29f588e99d316ca6c62f
Curacerto <curacerto@gmail.com>
2025-02-11 13:14:19 +0000

    Item domain

--- src/infrastructure/config/typeorm.config.ts
+++ src/infrastructure/config/typeorm.config.ts
@@ -1,6 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
-import { join } from 'path';
 
 export const getTypeOrmConfig = (
   configService: ConfigService,
@@ -11,7 +10,7 @@ export const getTypeOrmConfig = (
   username: configService.get('DATABASE_USER'),
   password: configService.get('DATABASE_PASSWORD'),
   database: configService.get('DATABASE_NAME'),
-  //entities: ['dist/domain/**/*.entity{.ts,.js}'],
-  entities: [join(__dirname, '..', '..', 'domain', '**', '*.entity{.ts,.js}')],
+  entities: ['dist/domain/**/*.entity{.ts,.js}'],
+  //entities: [join(__dirname, '..', '..', 'domain', '**', '*.entity{.ts,.js}')],
   synchronize: false,
 });
